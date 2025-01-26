b5d05f01a97533fe3626aeaf0aa1ae172c51a28f
Sohrab Arenja <sohrab-arenja8@hotmail.de>
2025-01-26 23:30:14 +0100

    render in Basket

--- script.js
+++ script.js
@@ -5,7 +5,6 @@ let prices = [];
 let amounts = [];
 
 
-
 function renderMenu() {
     let menuCard = document.getElementById("menus");
     menuCard.innerHTML = "";
@@ -26,10 +25,31 @@ function TemplateMenuCard(i) {
 }
 
 function addToBasket(i) {
-    foods.unshift(myDishes[i].name);
-    prices.unshift(myDishes[i].price);
-    amounts.unshift(myDishes[i].amount++);
+    document.getElementById("orderedFood").innerHTML += basketTemplate(i);
+    let food = myDishes[i].name;
+    foods.unshift(food);
+    let price = myDishes[i].price;
+    prices.unshift(price);
+    let amount = myDishes[i].amount++;
+    amounts.unshift(amount);
     console.log(foods);
     console.log(prices);
     console.log(amounts);
 }
+
+function basketTemplate(i) {
+    return `<div class="orderedFood" >
+                <h4>${myDishes[i].name}</h4>
+                <div class="amountSection">
+                    <p>${myDishes[i].amount}x</p>
+                    <p>${myDishes[i].price.toFixed(2)} €</p>
+                </div>
+                <div class="buttonSection">
+                    <button class="basketButton">-</button>
+                    <button class="basketButton">+</button>
+                </div>  
+            </div>`
+}
+
+
+
